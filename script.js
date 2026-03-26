a78b873ee7007c73ad3573ce9997941ae1ff2d0c
Lil Thalis <jacomossi.thalison@escola.pr.gov.br>
2026-03-26 11:19:05 -0300

    css

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-document.getElementById('openMenu').onclick = function() {
-    document.getElementById('sideMenu').style.width = "350px";
-}
+/* MENU TOGGLE */
 
-document.getElementById('closeMenu').onclick = function() {
-    document.getElementById('sideMenu').style.width = "0";
-}
+const menu = document.getElementById("menu");
+const openBtn = document.getElementById("openMenu");
+
+openBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+
+/* SCROLL ANIMATION */
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("visible");
+    }
+  });
+},{
+  threshold:0.2
+});
+
+document.querySelectorAll(".card")
+.forEach(el => observer.observe(el));
